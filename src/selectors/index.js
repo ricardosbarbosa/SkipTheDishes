ebd0b4f5448ba15b34dc8b527aa056d2642298f7
Ricardo Barbosa <rbrico@gmail.com>
2018-06-24 18:01:04 -0300

    clen up

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -6,6 +6,6 @@ const getOrderBy = (state) => state.orderBy
 export const getShoppingCartSorted = createSelector(
   [ getOrderBy, getShoppingList ],
   ( orderBy, shoppingList ) => {
-    return shoppingList.sort((a, b) => orderBy === "Name"? a.name > b.name : a.quantity > b.quantity)
+    return shoppingList.sort((a, b) => orderBy === "Name" ? a.name > b.name : a.quantity > b.quantity)
   }
 )
--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import api from '../middleware/api'
 import rootReducer from '../reducers'
 
 const configureStore = preloadedState => createStore(
